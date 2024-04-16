6e142fce4312b8cc79f877f22fff147c0c77298c
Joel Manga <jmanga86@gmail.com>
2024-04-16 12:24:44 +0300

    footer updated

--- src/Componets/Footer/Footer.jsx
+++ src/Componets/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
     <div className='footer'>
       <div className="footer-logo">
         <img src={footer_logo} alt="footer logo missing" />
-        <p>SHOPPER</p>
+        <p>Njiku Fashion</p>
       </div>
       <ul className="footer-links">
         <li>Company</li>
